cf9a237242ed959e50839c9da4345ec98ff854d2
Bobby Comet <bobbycometnovels@gmail.com>
2026-08-01 12:39:45 -0500

    Delete conkystudio directory

--- conkystudio/model/__init__.py
+++
@@ -1,7 +0,0 @@
-from conkystudio.model.project import Project, NodeInstance, Edge, CanvasSettings, SCHEMA_VERSION, new_id
-from conkystudio.model.theme_meta import ThemeMeta, THEME_META_FILENAME
-
-__all__ = [
-    "Project", "NodeInstance", "Edge", "CanvasSettings", "SCHEMA_VERSION", "new_id",
-    "ThemeMeta", "THEME_META_FILENAME",
-]
